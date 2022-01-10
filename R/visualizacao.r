06ded7c91d5d19a6bd02298659e8f53a96e76ea8
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2022-01-10 19:15:44 -0300

    Add e doc plot de interpolador

--- R/visualizacao.r
+++ R/visualizacao.r
@@ -68,6 +68,76 @@ plot.curvacolina <- function(x, tipo = c("3d", "2d"), print = TRUE, ...) {
     }
 }
 
-plot.interpolador <- function(x, hl, pot, print = TRUE, ...) {
-    NA
+#' Visualizacao De Interpolacoes
+#' 
+#' Funcao para visualizacao dos modelos ajustados atraves de \code{interpolador}
+#' 
+#' Os argumentos \code{hl} e \code{pot} devem ser fornecidos como um numero, indicando quantos 
+#' cortes no eixo de queda e potencia, respectivamente, devem ser utilizados na geracao de uma grade 
+#' a qual interpolar na superficie suavizada. Estes pontos serao entao utilizados para plot.
+#' 
+#' @param x objeto \code{interpolador} retornado pela funcao homonima
+#' @param add_colina booleano indicando se os pontos da colina original tambem devem ser plotados
+#' @param tipo um de \code{c("3d", "2d")} indicando o tipo de grafico desejado
+#' @param hl,pot vetores definindo grade amostrada. Ver Detalhes
+#' @param print booleano indicando se o plot deve ser exibido. Caso \code{print = FALSE} o objeto
+#'     sera retornado silenciosamente
+#' 
+#' @return Se \code{tipo = "3d"} um objeto \code{plotly} contendo o plot 3d; se \code{tipo = "2d"}
+#'     um objeto \code{ggplot} contendo o plot 2d. Em ambos os casos o grafico so sera exibido ao
+#'     usuario caso \code{print = TRUE} (o padrao).
+#' 
+#' @family interpolador
+#' 
+#' @import data.table
+#' @importFrom plotly plot_ly `%>%` add_markers add_surface layout hide_colorbar hide_legend
+#' @importFrom ggplot2 ggplot aes geom_point scale_color_viridis_d labs theme_bw guides guide_legend
+#'     geom_raster scale_fill_viridis_c
+#' 
+#' @export 
+
+plot.interpolador <- function(x, add_colina = TRUE, tipo = c("3d", "2d"), hl = 200, pot = 200,
+    print = TRUE, ...) {
+
+    tipo <- match.arg(tipo)
+
+    if(add_colina) colina <- copy(getcolina(x)$CC) else colina <- data.table(hl = NA, pot = NA, rend = 0)
+
+    dsurf <- geragrade(getcolina(x), hl, pot)
+    dsurf[, rend := predict(x, dsurf)]
+
+    if(tipo == "3d") {
+        p <- plot_ly() %>%
+            add_markers(x = colina$hl, y = colina$pot, z = colina$rend,
+                type = "scatter3d", name = "colina") %>%
+            add_surface(x = unique(dsurf$hl), y = unique(dsurf$pot),
+                z = t(data.matrix(dcast(dsurf, hl ~ pot, value.var = "rend"))),
+                name = "interpolacao") %>%
+            layout(scene = list(
+                xaxis = list(title = list(text = "Queda Liquida")),
+                yaxis = list(title = list(text = "Potencia")),
+                zaxis = list(title = list(text = "Rendimento")))
+            ) %>%
+            hide_legend() %>%
+            hide_colorbar()
+
+        if(print) print(p)
+
+        invisible(p)
+    } else {
+        colina[, rend := factor(paste0(formatC(rend, format = "f", digits = 3), "%"))]
+        colina <- colina[complete.cases(colina), ]
+        dsurf <- dsurf[complete.cases(dsurf)]
+        p <- ggplot() +
+            geom_raster(data = dsurf, aes(hl, pot, fill = rend)) +
+            geom_point(data = colina, aes(hl, pot), color = "blue") +
+            scale_fill_viridis_c(name = "Rendimento", na.value = NA) +
+            labs(x = "Queda Liquida", y = "Potencia") +
+            theme_bw() +
+            guides(color = guide_legend(ncol = 1))
+
+        if(print) print(p)
+
+        invisible(p)
+    }
 }
