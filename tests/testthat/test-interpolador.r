5b961ce244dd7c302073d393f1fb3e203eadb480
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2022-01-12 15:48:12 -0300

    Add testes de thinplate

--- tests/testthat/test-interpolador.r
+++ tests/testthat/test-interpolador.r
@@ -33,3 +33,21 @@ test_that("Modelagem por Tensor Product", {
     p2d <- plot(interp, tipo = "2d", print = FALSE)
     expect_equal(class(p2d), c("gg", "ggplot"))
 })
+
+test_that("Modelagem por Thin Plate", {
+    interp <- interpolador(colinadummy, "thinplate", taxa_reducao = 2)
+
+    expect_equal(class(interp), c("thinplate", "interpolador"))
+    expect_equal(length(interp), 2)
+    expect_equal(names(interp), c("superficie", "colina"))
+
+    expect_equal(getcolina.thinplate(interp), colinadummy)
+
+    expect_snapshot_value(predict(interp, geragrade(colinadummy, 20, 20)), style = "json2")
+
+    p3d <- plot(interp, hl = 10, pot = 10, print = FALSE)
+    expect_equal(class(p3d), c("plotly", "htmlwidget"))
+
+    p2d <- plot(interp, hl = 10, pot = 10, tipo = "2d", print = FALSE)
+    expect_equal(class(p2d), c("gg", "ggplot"))
+})
