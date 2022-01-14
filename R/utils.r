5ac57f6dfa7941521a7379afef4f36b093e94925
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2022-01-14 12:50:56 -0300

    Flexibiliza argumentos de geragrade

--- R/utils.r
+++ R/utils.r
@@ -1,15 +1,17 @@
 ######################################## FUNCOES UTILITARIAS #######################################
 
-#' Gera Grade \code{nhl} x \code{npot} No Dominio Da Curva Colina
+#' Gera Grade \code{dhl} x \code{dpot} No Dominio Da Curva Colina
 #' 
 #' Funcao auxiliar para gerar grades de pontos nos quais interpolar a curva colina
 #' 
 #' @param colina objeto da classe \code{curvacolina} (retornado pelas funcoes de leitura)
-#' @param nhl,npot numero de divisoes no eixo de queda liquida e potencia, respectivamente
+#' @param dhl,dpot numero de divisoes no eixo de queda liquida e potencia, respectivamente. Tambem
+#'     podem ser fornecidos vetores indicando as posicoes das divisoes de queda e potencia
 #' 
 #' @examples
 #' 
-#' grade <- geragrade(colinadummy, 20, 20)
+#' grade1 <- geragrade(colinadummy, 20, 20)
+#' grade2 <- geragrade(colinadummy, 40:60, seq(150, 400, by = 10))
 #' 
 #' \dontrun{
 #' plot(colinadummy, "2d") + ggplot2::geom_point(data = grade, aes(hl, pot), col = 2)
@@ -23,18 +25,18 @@
 #' 
 #' @export
 
-geragrade <- function(colina, nhl, npot) UseMethod("geragrade", colina)
+geragrade <- function(colina, dhl, dpot) UseMethod("geragrade", colina)
 
 #' @export
 
-geragrade.data.table <- function(colina, nhl, npot) {
+geragrade.data.table <- function(colina, dhl, dpot) {
 
     hl <- pot <- NULL
 
-    grade <- expand.grid(
-        hl  = colina[, seq(min(hl),  max(hl),  length.out = nhl)],
-        pot = colina[, seq(min(pot), max(pot), length.out = npot)]
-    )
+    if(length(dhl) == 1L) hl <- colina[, seq(min(hl),  max(hl),  length.out = dhl)] else hl <- dhl
+    if(length(dpot) == 1L) pot <- colina[, seq(min(pot),  max(pot),  length.out = dpot)] else pot <- dpot
+
+    grade <- expand.grid(hl = hl, pot = pot)
 
     grade <- as.data.table(grade)
 
@@ -43,18 +45,18 @@ geragrade.data.table <- function(colina, nhl, npot) {
 
 #' @export
 
-geragrade.data.frame <- function(colina, nhl, npot) {
+geragrade.data.frame <- function(colina, dhl, dpot) {
 
-    grade <- geragrade.data.table(as.data.table(colina), nhl, npot)
+    grade <- geragrade.data.table(as.data.table(colina), dhl, dpot)
 
     return(grade)
 }
 
 #' @export
 
-geragrade.curvacolina <- function(colina, nhl, npot) {
+geragrade.curvacolina <- function(colina, dhl, dpot) {
 
-    grade <- geragrade(colina$CC, nhl, npot)
+    grade <- geragrade(colina$CC, dhl, dpot)
 
     return(grade)
 }
